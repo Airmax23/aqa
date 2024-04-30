2243fb98663e1e1d8a77b3c4e7e9c87adcfcedd9
Anton Trubin <a.trubin@tontilagunamobile.com>
2024-04-30 13:52:16 +0300

    fix boolean

--- jsBasics/task4.js
+++ jsBasics/task4.js
@@ -1,12 +1,17 @@
 
-let age = 18;
+//let age = 18;
 
 
-if (age < 18) {
-    console.log('The person is an adult.');
-} else {
-    console.log('The person is not of legal age.');
-}
+//if (age => 18) {
+//    console.log('The person is an adult.');
+//} else {
+//    console.log('The person is not of legal age.');
+//}
 
 
-console.log(age);
+//console.log(age);
+
+let age = 17;
+let isAdult = age >= 18;
+
+console.log(isAdult);
